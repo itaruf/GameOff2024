97691c2996b56a5fcd8ebc01b968d02bc0c7c9a5
Imane Taruf <79866301+itaruf@users.noreply.github.com>
2024-12-08 15:48:00 +0900

    feat: add an Interact Component interface provider to reduce overhead

---
+++ Source/GameOff2024/Interfaces/IInteractProvider.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IInteractProvider.generated.h"
+
+class UInteractComponent;
+// This class does not need to be modified.
+UINTERFACE(BlueprintType)
+class UInteractProvider : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class GAMEOFF2024_API IInteractProvider
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interact", DisplayName = "Get Interact Component")
+	UInteractComponent* GetInteractComponent() const;
+};
