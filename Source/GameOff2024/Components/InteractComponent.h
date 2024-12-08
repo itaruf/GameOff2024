46f675cac63f5940a84b67f3c0e601160982d45f
Imane Taruf <79866301+itaruf@users.noreply.github.com>
2024-12-08 15:46:51 +0900

    feat: add an Interact Component to handle interactions with Objects/NPCs
    to be blueprint derived and attached to manually

---
+++ Source/GameOff2024/Components/InteractComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InteractComponent.generated.h"
+
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), DisplayName="InteractComponent")
+class GAMEOFF2024_API UInteractComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInteractComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
