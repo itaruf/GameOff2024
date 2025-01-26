5197e5adf3a26b906aaa7f4d0bf9bcce495aee95
Imane Taruf <79866301+itaruf@users.noreply.github.com>
2025-01-26 20:47:43 +0900

    feat: add a custom Save Game class derived from USaveGame

---
+++ Source/GameOff2024/Save/CustomSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "CustomSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class GAMEOFF2024_API UCustomSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
