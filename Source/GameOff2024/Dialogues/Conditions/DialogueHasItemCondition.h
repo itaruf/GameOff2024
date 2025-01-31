977d9909e2ad34498937f31f7093719a7bfa2532
Imane Taruf <79866301+itaruf@users.noreply.github.com>
2025-01-31 22:51:41 +0900

    feat: add a dialogue condition (bool) to check if an inventory has the asked item

---
+++ Source/GameOff2024/Dialogues/Conditions/DialogueHasItemCondition.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Conditionals/DialogueConditionBool.h"
+#include "DialogueHasItemCondition.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class GAMEOFF2024_API UDialogueHasItemCondition : public UDialogueConditionBool
+{
+	GENERATED_BODY()
+	
+};
