3be7288b532ec1ff4b2d3a9aba32f24963e8f932
itaruf <79866301+itaruf@users.noreply.github.com>
2025-02-03 01:12:28 +0900

    refactor(BPQ_IsQuestCompleted): get the quest's completion state from the GameInstance

---
+++ Source/GameOff2024/Dialogues/Queries/DialogueQuestActiveQuery.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueNoParamQueryBool.h"
+#include "DialogueQuestActiveQuery.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class GAMEOFF2024_API UDialogueQuestActiveQuery : public UDialogueNoParamQueryBool
+{
+	GENERATED_BODY()
+};
